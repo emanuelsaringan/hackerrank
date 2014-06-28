9de3827f38415fc64769992313bfbfb136edbb5a
Emanuel Saringan <dev@lambdageek.com>
2014-06-28 23:55:26 +0800

    Edited FindTheMedian solution

--- FindTheMedian/Solution.java
+++ FindTheMedian/Solution.java
@@ -3,42 +3,18 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+
         int n = in.nextInt();
-        PriorityQueue<Integer> left = new PriorityQueue<Integer>(n, Collections.reverseOrder());
-        PriorityQueue<Integer> right = new PriorityQueue<Integer>(n);
-        
-        left.offer(in.nextInt());
-        for (int i = 1; i < n; i++) {
-            int num = in.nextInt();
-            
-            if (right.isEmpty()) {
-                if (num <= left.peek()) {
-                    right.offer(left.poll());
-                    left.offer(num);
-                } else {
-                    right.offer(num);
-                }
-                
-                continue;
-            }
-            
-            if (left.size() == right.size()) {
-                if (num >= right.peek()) {
-                    left.offer(right.poll());
-                    right.offer(num);
-                } else {
-                    left.offer(num);
-                }
-            } else {
-                if (num >= right.peek()) {
-                    right.offer(num);
-                } else {
-                    right.offer(left.poll());
-                    left.offer(num);
-                }
-            }
+        for (int i = 0; i < n; i++) {
+            pq.offer(in.nextInt());
         }
-        
-        System.out.println(left.peek());
+
+        int limit = n >> 1;
+        for (int i = 0; i < limit; i++) {
+            pq.poll();
+        }
+
+        System.out.println(pq.peek());
     }
 }
